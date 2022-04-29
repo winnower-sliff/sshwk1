fd480c035219072ff0bd0b4b827717c57dac7e76
winnower-sliff <1427035192@qq.com>
2022-04-30 03:44:07 +0800

    iat

--- Detector_IAT.py
+++ Detector_IAT.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*- 
-
+# -*- coding: utf-8 -*-
 
 import os
 import sys
@@ -14,8 +13,9 @@ from sklearn.feature_extraction import FeatureHasher
 
 import pefile
 
+
 # 获取文件相关数据
-def get_iat_features(path,hasher):
+def get_iat_features(path, hasher):
     # extract strings from binary file using regular expressions
     # chars = r" -~"
     # min_length = 5
@@ -30,18 +30,18 @@ def get_iat_features(path,hasher):
     # string_features = {}
     # for string in strings:
     #     string_features[string] = 1
-    iat_features={}
+    iat_features = {}
     try:
         pe = pefile.PE(path)
     except pefile.PEFormatError:
         pass
     else:
-        if hasattr(pe,'DIRECTORY_ENTRY_IMPORT'):
+        if hasattr(pe, 'DIRECTORY_ENTRY_IMPORT'):
             for entry in pe.DIRECTORY_ENTRY_IMPORT:
                 # print(entry.dll)
                 for imp in entry.imports:
                     # print('\t', hex(imp.address), imp.name,type(imp.name))
-                    iat_features[str(imp.name)]=1
+                    iat_features[str(imp.name)] = 1
 
     # hash the features using the hashing trick
     hashed_features = hasher.transform([iat_features])
@@ -52,47 +52,57 @@ def get_iat_features(path,hasher):
     hashed_features = hashed_features[0]
     #print("Finished Extract String Feature")
     # return hashed string features
-    print("Extracted {0} IATs from {1}".format(len(iat_features),path))
+    print("Extracted {0} IATs from {1}".format(len(iat_features), path))
     return hashed_features
+
 
 # 扫描并判断正邪
 def scan_file(path):
     # scan a file to determine if it is malicious or benign
     if not os.path.exists("saved_detector.pkl"):
-        print("It appears you haven't trained a detector yet!  Do this before scanning files.")
+        print(
+            "It appears you haven't trained a detector yet!  Do this before scanning files."
+        )
         sys.exit(1)
-    with open("saved_detector.pkl","rb") as saved_detector:
+    with open("saved_detector.pkl", "rb") as saved_detector:
         classifier, hasher = pickle.load(saved_detector)
-    features = get_iat_features(path,hasher)
-    result_proba = classifier.predict_proba([features])[:,1]
+    features = get_iat_features(path, hasher)
+    result_proba = classifier.predict_proba([features])[:, 1]
     # if the user specifies malware_paths and benignware_paths, train a detector
     if result_proba > 0.5:
-        print("It appears this file is malicious!",result_proba)
+        print("It appears this file is malicious!", result_proba)
     else:
-        print("It appears this file is benign.",result_proba)
+        print("It appears this file is benign.", result_proba)
+
 
 # 利用数据训练分类器
-def train_detector(benign_path,malicious_path,hasher):
+def train_detector(benign_path, malicious_path, hasher):
     # train the detector on the specified training data
     def get_training_paths(directory):
         targets = []
         for path in os.listdir(directory):
-            targets.append(os.path.join(directory,path))
+            targets.append(os.path.join(directory, path))
         return targets
+
     malicious_paths = get_training_paths(malicious_path)
     benign_paths = get_training_paths(benign_path)
     print("Begin Training...")
-    X = [get_iat_features(path,hasher) for path in malicious_paths + benign_paths]
-    y = [1 for i in range(len(malicious_paths))] + [0 for i in range(len(benign_paths))]
+    X = [
+        get_iat_features(path, hasher)
+        for path in malicious_paths + benign_paths
+    ]
+    y = [1 for i in range(len(malicious_paths))
+         ] + [0 for i in range(len(benign_paths))]
     classifier = RandomForestClassifier(64)
-    classifier.fit(X,y)
+    classifier.fit(X, y)
     print("End Training...")
     print("Begin Saving Models...")
-    pickle.dump((classifier,hasher),open("saved_detector.pkl","wb+"))
+    pickle.dump((classifier, hasher), open("saved_detector.pkl", "wb+"))
     print("End Saving Models...")
 
+
 # 生成CV图像
-def cv_evaluate(X,y,hasher):
+def cv_evaluate(X, y, hasher):
     # use cross-validation to evaluate our model
     import random
     from sklearn import metrics
@@ -100,20 +110,20 @@ def cv_evaluate(X,y,hasher):
     from sklearn.model_selection import KFold
     X, y = numpy.array(X), numpy.array(y)
     fold_counter = 0
-    for train, test in KFold(2,shuffle=True).split(X,y):
+    for train, test in KFold(2, shuffle=True).split(X, y):
         training_X, training_y = X[train], y[train]
         test_X, test_y = X[test], y[test]
         classifier = RandomForestClassifier(64)
-        classifier.fit(training_X,training_y)
-        scores = classifier.predict_proba(test_X)[:,-1]
+        classifier.fit(training_X, training_y)
+        scores = classifier.predict_proba(test_X)[:, -1]
         fpr, tpr, thresholds = metrics.roc_curve(test_y, scores)
-        pyplot.semilogx(fpr,tpr,label="Fold number {0}".format(fold_counter))
+        pyplot.semilogx(fpr, tpr, label="Fold number {0}".format(fold_counter))
         #pyplot.semilogx(fpr,tpr,label="ROC curve".format(fold_counter))
         fold_counter += 1
-        with open("proba.log","w") as f:
+        with open("proba.log", "w") as f:
             scores.sort()
             for s in scores:
-                f.write(str(s)+"\n")
+                f.write(str(s) + "\n")
     pyplot.xlabel("detector false positive rate")
     pyplot.ylabel("detector true positive rate")
     pyplot.title("Detector ROC curve")
@@ -122,26 +132,40 @@ def cv_evaluate(X,y,hasher):
     pyplot.grid()
     pyplot.show()
 
+
 # 获取正邪文件相关数据
-def get_training_data(benign_path,malicious_path,hasher):
+def get_training_data(benign_path, malicious_path, hasher):
     def get_training_paths(directory):
         targets = []
         for path in os.listdir(directory):
-            targets.append(os.path.join(directory,path))
+            targets.append(os.path.join(directory, path))
         return targets
+
     malicious_paths = get_training_paths(malicious_path)
     benign_paths = get_training_paths(benign_path)
-    X = [get_iat_features(path,hasher) for path in malicious_paths + benign_paths]
-    y = [1 for i in range(len(malicious_paths))] + [0 for i in range(len(benign_paths))]
+    X = [
+        get_iat_features(path, hasher)
+        for path in malicious_paths + benign_paths
+    ]
+    y = [1 for i in range(len(malicious_paths))
+         ] + [0 for i in range(len(benign_paths))]
     return X, y
+
 
 def main():
     defaultpath = "./data"
     parser = argparse.ArgumentParser("get windows object vectors for files")
-    parser.add_argument("--malware_paths",default=os.path.join(defaultpath,"malware"),help="Path to malware training files")
-    parser.add_argument("--benignware_paths",default=os.path.join(defaultpath,"benignware"),help="Path to benignware training files")
-    parser.add_argument("--scan_file_path",default=None,help="File to scan")
-    parser.add_argument("--evaluate",default=False,action="store_true",help="Perform cross-validation")
+    parser.add_argument("--malware_paths",
+                        default=os.path.join(defaultpath, "malware"),
+                        help="Path to malware training files")
+    parser.add_argument("--benignware_paths",
+                        default=os.path.join(defaultpath, "benignware"),
+                        help="Path to benignware training files")
+    parser.add_argument("--scan_file_path", default=None, help="File to scan")
+    parser.add_argument("--evaluate",
+                        default=False,
+                        action="store_true",
+                        help="Perform cross-validation")
 
     args = parser.parse_args()
 
@@ -150,15 +174,17 @@ def main():
     if args.scan_file_path:
         scan_file(args.scan_file_path)
     elif args.malware_paths and args.benignware_paths and not args.evaluate:
-        train_detector(args.benignware_paths,args.malware_paths,hasher)
+        train_detector(args.benignware_paths, args.malware_paths, hasher)
     elif args.malware_paths and args.benignware_paths and args.evaluate:
-        X, y = get_training_data(args.benignware_paths,args.malware_paths,hasher)
-        cv_evaluate(X,y,hasher)
+        X, y = get_training_data(args.benignware_paths, args.malware_paths,
+                                 hasher)
+        cv_evaluate(X, y, hasher)
     else:
         print("[*] You did not specify a path to scan," \
             " nor did you specify paths to malicious and benign training files" \
             " please specify one of these to use the detector.\n")
         parser.print_help()
+
 
 if __name__ == '__main__':
     main()
